2f936d9e200941ae79c1c36a883e2f3bfed1a11f
romnick <romnicksanjuan9@gmail.com>
2025-01-09 17:52:58 +0800

    upds

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ mongoose.connect(MOGNG_DB)
 // http://localhost:5173
 // https://e-commerce-x81l.vercel.app
 app.use(cors({
-    origin: ' http://localhost:5173', // Allow requests from the React app (Vite default port)
+    origin: 'https://e-commerce-x81l.vercel.app', // Allow requests from the React app (Vite default port)
     methods: ['GET', 'POST','PUT','DELETE'],       // Allow specific HTTP methods
     credentials: true,
 }))
